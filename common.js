a5c8ea300fcb48a7752103c8b696d2fd4f31c4a0
OLspectre <ossi9910@gmail.com>
2024-12-17 18:36:29 +0100

    Set up basic HTML for each page. Added and styled links to be placed accordingly.
    Home link in each sub-page is created using JS.
    
    <title> element also adjustedfor pages

---
+++ common.js
@@ -0,0 +1,7 @@
+
+
+// Home menu link
+let homeLink = document.createElement("a");
+homeLink.textContent = "Home"
+homeLink.setAttribute("href", "../index.html")
+document.querySelector("nav").appendChild(homeLink);
