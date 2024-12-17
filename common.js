f7edf19a17c4ad3d2332a6d8e7c95e93ad527ed0
OLspectre <ossi9910@gmail.com>
2024-12-17 23:33:22 +0100

    Created basic controls layout, mainly controls created via JS. Determined correct styling via common.css for controls.
    Trying to follow similar structure for every page. Next part - add grid system

--- common.js
+++ common.js
@@ -5,3 +5,19 @@ let homeLink = document.createElement("a");
 homeLink.textContent = "Home"
 homeLink.setAttribute("href", "../index.html")
 document.querySelector("nav").appendChild(homeLink);
+
+
+let numberQuestion = document.createElement("div");
+document.getElementById("creator").appendChild(numberQuestion);
+numberQuestion.textContent = "How many numbers in the grid?"
+let createNumberInput = document.createElement("input");
+document.getElementById("creator").appendChild(createNumberInput);
+
+let createButton = document.createElement("button");
+createButton.textContent = "Create"
+document.getElementById("creator").appendChild(createButton);
+
+
+
+
+
